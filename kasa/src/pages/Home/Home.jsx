a09217f092c9bb1584ee0079eaeec867ff3c5c3f
EllanMXM <maxime.jamard@gmail.com>
2023-04-03 18:39:38 +0200

    AJOUT FONCTION HOME + COMPONENT BANNER_GALLERY_FOOTER

---
+++ kasa/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../../components/banner/Banner';
+import Footer from '../../components/footer/Footer';
+import Gallery from '../../components/gallery/Gallery';
+
+export default function Home(){
+    return(
+        <div> 
+            <Banner />
+            <Gallery/>
+            <Footer />
+        </div>
+    )
+}
