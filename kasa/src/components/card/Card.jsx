ee644586799b4c98303a79e1fa8cc8ea78fa6475
EllanMXM <maxime.jamard@gmail.com>
2023-04-17 10:03:28 +0200

    MODIFICATION NAME LINK

--- kasa/src/components/card/Card.jsx
+++ kasa/src/components/card/Card.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Card({id, title, cover}) {
 
 	return (
-		<Link to={`/Hebergement/${id}`} className="galleryCard">
+		<Link to={`/Logements/${id}`} className="galleryCard">
 			<img className='cardImage' src={cover} alt={title} />
 			<h3>{title}</h3>	
 		</Link>
