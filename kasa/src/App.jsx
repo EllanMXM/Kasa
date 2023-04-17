d97bed5d8bf02acc43a0812b22a8129807fd34dc
EllanMXM <maxime.jamard@gmail.com>
2023-04-17 10:00:52 +0200

    AJOUT ROUTES LOGEMENTS + APROPOS

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/header/Header';
-import Logements from './pages/Logements/Logements';
+import Location from './components/location/Location';
+import APropos from './pages/Apropos/Apropos';
+import Logements from './pages/logements/Logements';
+
 
 function App () {
-    return (
-        <div className='App'> 
-        <Header />
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/Logements" element={<Logements/>} />
-        </Routes>
-        </div>
-    );
+  return (
+    <div className='App'> 
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Logements/:id" element={<Location />} />
+        <Route path="/Logements" element={<Logements />} />
+        <Route path="/Apropos" element={<APropos />}/>
+      </Routes>
+    </div>
+  );
 };
 
 export default App
