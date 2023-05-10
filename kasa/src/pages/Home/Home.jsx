1aa06d81dd1e1db61852bf399bed798839db3af1
EllanMXM <maxime.jamard@gmail.com>
2023-05-10 18:25:57 +0200

    AJOUT TITLE WITH 'USEEFFECT'

--- kasa/src/pages/Home/Home.jsx
+++ kasa/src/pages/Home/Home.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect }  from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../../components/banner/Banner';
 import Footer from '../../components/footer/Footer';
 import Gallery from '../../components/gallery/Gallery';
 
-export default function Home(){
-    
-    useEffect(() => {
-        document.title = "Home";
-    }, []);
+export default function Home() {
+  useEffect(() => {
+    document.title = 'Home';
+  }, []);
 
-    return(
-        <div> 
-            <Banner />
-            <Gallery/>
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Banner />
+      <Gallery />
+      <Footer />
+    </div>
+  );
 }
