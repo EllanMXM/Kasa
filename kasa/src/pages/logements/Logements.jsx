eace671bb1e8d8ba50a64ebff3926460b5646b8e
EllanMXM <maxime.jamard@gmail.com>
2023-05-10 18:26:40 +0200

    MOFIF FUNCTION LOGEMENT

--- kasa/src/pages/logements/Logements.jsx
+++ kasa/src/pages/logements/Logements.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Data from '../../data/data.json'
-import './logements.css'
+import React from 'react';
+import Data from '../../data/data.json';
+import './logements.css';
 import Location from '../../components/location/Location';
 
 function Logements() {
+
   return (
     <div className='logements'>
       {Data.map(({ id, title, cover }) => (
         <Location
+          key={id}
           id={id}
           title={title}
           cover={cover}
         />
       ))}
     </div>
-  )
+  );
 }
 
 export default Logements;
