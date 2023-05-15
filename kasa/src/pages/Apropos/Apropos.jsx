5ce759b1a8856ae4cde513b50ae21fcf30830344
EllanMXM <maxime.jamard@gmail.com>
2023-05-15 18:03:21 +0200

    MODIF FONCTION APROPOS

--- kasa/src/pages/Apropos/Apropos.jsx
+++ kasa/src/pages/Apropos/Apropos.jsx
@@ -7,37 +7,28 @@ import Footer from "../../components/footer/Footer";
 
 
 function APropos() {
- 
   useEffect(() => {
     document.title = "A Propos";
-}, []);
+  }, []);
 
-    return (
-      <div className="about">
-        <div id="aboutPage">
-          <img src={img} title=" " alt="paysage" className="banner_apropos"/>
-        </div>
-          <section className="aboutCollapse">    
-             
-              {Script.map(data => { 
-                  return(
-                    <div className="aboutCollapse__div"> 
-                        <Collapse
-                        key={data.id} 
-                        title={data.label} 
-                        description={data.content}
-                        
-                        /> 
-                    </div> 
-                  )
-              })}
-            
-          </section>
-
-       
-       <Footer/>
+  return (
+    <div className="about">
+      <div id="aboutPage">
+        <img src={img} title=" " alt="paysage" className="banner_apropos" />
       </div>
-      ); 
-    }
-    
-    export default APropos;
+      <section className="aboutCollapse">
+        {Script.map((data) => (
+          <div className="aboutCollapse__div" key={data.id}>
+            <Collapse
+              title={data.label}
+              description={data.content}
+            />
+          </div>
+        ))}
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default APropos;
